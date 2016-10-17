4132f602557b832fa721b9c25974c99a59d20473
Kirill Meng <kimeng@htwg-konstanz.de>
2016-10-17 12:22:19 +0200

    insert prototypes in header file

---
+++ L1/ulstack.h
@@ -0,0 +1,11 @@
+typedef struct {
+    unsigned long *elems;
+    unsigned int logLength;
+    unsigned int allocLength;
+} ulstack;
+
+void ULStackNew(ulstack *s);
+void ULStackDispose(ulstack *s);
+void ULStackPush(ulstack *s, unsigned long value);
+unsigned long ULStackPop(ulstack *s);
+unsigned int GetULStackNumberOfElements(ulstack *s);
