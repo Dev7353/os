d9409c8d66042d121f4f71aee413977ae54fccb6
Kirill Meng <kimeng@hotmail.de>
2016-11-09 15:34:36 +0100

    add GenStackNew function

--- L2/genstacklib.c
+++ L2/genstacklib.c
@@ -1,4 +1,4 @@
-#include "genstackstack.h"
+#include "genstacklib.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -7,40 +7,26 @@ int stack_created = 0;
 
 void GenStackNew(genStack *s, int elemSize, void(*freefn)(void*))
 {
-    s->elems = (unsigned long *) malloc(STACK_CAPACITY * sizeof(unsigned long));
+    s->elems = (void *) malloc(GenStackInitialAllocationSize * elemSize);
     assert(s->elems != NULL);
     
     s->logLength = 0; 
-    s->allocLength = STACK_CAPACITY;
+    s->allocLength = GenStackInitialAllocationSize;
     
 }
 void GenStackDispose(genStack *s)
 {
-    assert(s->allocLength != 0);
-    free(s->elems);
-    s->elems = NULL;
-    s->allocLength = 0;
-    s->logLength = 0;
+
     
 }
 void GenStackPush(genStack *s, const void *elemAddr)
 {
-    if(s->logLength == s->allocLength)
-    {
-        s->elems = (unsigned long *) realloc(s->elems, s->allocLength * 2 *  sizeof(unsigned long));
-	s->allocLength *= 2;
-	assert(s->elems != NULL);
-    } 
-    s->elems[s->logLength]= value;
-    s->logLength++;
+
     
 }
-int GenStackPop(genStack *s, const void *elemAddr)
+void GenStackPop(genStack *s, void *elemAddr)
 {
-    unsigned long value = s->elems[s->logLength-1];
-    s->logLength--;
-    
-    return value;
+	
 }
 bool GenStackEmpty(const genStack *s)
 {
