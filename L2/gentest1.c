82ba652a01b83bac90c24a52e0c88160a006e416
Kirill Meng <kimeng@hotmail.de>
2016-11-10 09:11:05 +0100

    add test from exercise example

--- L2/gentest1.c
+++ L2/gentest1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include <"genstacklib.h">
+#include "genstacklib.h"
 
 int main(void)
 {
 	int val; 
 	genStack IntegerStack;
 
-	GenStackNew(&IntegerStack, sizeof(int), Null); 
+	GenStackNew(&IntegerStack, sizeof(int), NULL); 
 
 	for (val = 0; val < 6; val++)
 	  GenStackPush(&IntegerStack, &val);
+	  /*Dereferenzierung u zu chekcn ob die werte tatsächlich gepusht wurden*/
+		int* ptr;
+		ptr = (int*) &IntegerStack.elems;
+		printf("Round %d, Push %i\n", val, ptr[val]);
 
 	while(!GenStackEmpty(&IntegerStack)){ 
 	  GenStackPop(&IntegerStack, &val); 
