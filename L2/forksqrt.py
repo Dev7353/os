7c9939cd9c4853010019281cdff4d601bfe64d08
Tan Le-Minh <taleminh@htwg-konstanz.de>
2016-11-11 20:36:14 +0100

    Add forksqrt.c file, adjust makefile

--- L2/forksqrt.py
+++ L2/forksqrt.py
@@ -4,6 +4,10 @@ def sqrt2(value, debug):
     loops = 100
     tolerance = 1.e-14
     xn = start
+    # Assert input is a number and bigger than 0
+    assert isinstance(value, int), 'Input must be a number!'
+    assert value > 0, 'Input must be bigger than 0!'
+
     if(debug is True):
         print("Testing with var = {:.14f}".format(value))
     for i in range(0, loops):
@@ -18,7 +22,7 @@ def sqrt2(value, debug):
 
 
 def main():
-    sqrt2(2, True)
+    sqrt2(21213124324233, True)
 
 if __name__ == "__main__":
     main()
