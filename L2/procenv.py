bf3689cfc63b4737bcde3195373b781f4e4e3143
Tan Le-Minh <taleminh@htwg-konstanz.de>
2016-11-11 20:37:05 +0100

    Catch error in procenv

--- L2/procenv.py
+++ L2/procenv.py
@@ -14,25 +14,31 @@ def pretty_print(d):
 
 
 def get_env():
-    return OrderedDict([("CWD:", os.getcwd()), ("Home:", os.environ['HOME']),
-                        ("PWD:", os.path.abspath(os.path.join(os.getcwd(), os.pardir))),
-                        ("Path:", os.environ['PATH']), ("Shell:", os.environ['SHELL']),
-                        ("User:", os.environ['USER']), ("UID:", os.getuid()),
-                        ("GID:", os.getgid()), ("VEnv:", os.environ['VIRTUAL_ENV'])])
+    try:
+        return OrderedDict([("CWD:", os.getcwd()), ("Home:", os.environ['HOME']),
+                            ("PWD:", os.path.abspath(
+                                os.path.join(os.getcwd(), os.pardir))),
+                            ("Path:", os.environ[
+                             'PATH']), ("Shell:", os.environ['SHELL']),
+                            ("User:", os.environ['USER']
+                             ), ("UID:", os.getuid()),
+                            ("GID:", os.getgid()), ("VEnv:", os.environ['VIRTUAL_ENV'])])
+    except (KeyError, OSError, AttributeError):
+        print("A mistake occured. Likely a key wasn't found.")
 
 
 def get_tree():
     dict = OrderedDict()
     for root, dirs, files in os.walk('/proc/' + str(os.getpid())):
-            for file in files:
-                if 'task' in dirs:
-                    dirs.remove('task')
-                if file.endswith('comm'):
-                    process_name_file = open(os.path.join(root, 'comm'))
-                    process_name = process_name_file.read()
-                    stat = open(os.path.join(root, 'stat')).readline()
-                    status = stat.split(' ')[2]
-                    dict.update({'0': (os.getpid(), process_name.strip(), status)})
+        for file in files:
+            if 'task' in dirs:
+                dirs.remove('task')
+            if file.endswith('comm'):
+                process_name_file = open(os.path.join(root, 'comm'))
+                process_name = process_name_file.read()
+                stat = open(os.path.join(root, 'stat')).readline()
+                status = stat.split(' ')[2]
+                dict.update({'0': (os.getpid(), process_name.strip(), status)})
     # dict.update({'1': (os.getppid(), ' ', ' ')})
     current_pid = os.getpid()
     i = 1
@@ -49,7 +55,7 @@ def get_tree():
                     stat = open(os.path.join(root, 'stat')).readline()
                     status = stat.split(' ')[2]
                     dict.update({str(i): (parent_pid.strip(),
-                                process_name.strip(), status)})
+                                          process_name.strip(), status)})
         i = i + 1
     return dict
 
@@ -74,7 +80,8 @@ def usage():
 def print_tree(d):
     x = PrettyTable(["PID", "Name", "Status"])
     x.align["PID"] = "l"  # Left align city names
-    x.padding_width = 1  # One space between column edges and contents (default)
+    # One space between column edges and contents (default)
+    x.padding_width = 1
     for i in range(0, len(d)):
         values = list(d.values())[i]
         x.add_row([values[0], values[1], values[2]])
