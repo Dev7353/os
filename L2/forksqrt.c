053d238b088b0a1e77e84f802a4b900aac1bebc2
Tan Le-Minh <taleminh@htwg-konstanz.de>
2016-11-12 19:17:54 +0100

    Make forksqrt.c read array of numbers

--- L2/forksqrt.c
+++ L2/forksqrt.c
@@ -10,6 +10,7 @@ typedef struct
     int start;
     int loops;
     float tolerance;
+    int numbers[3];
 } configuration;
 
 static int handler(void* sqrt2, const char* section, const char* name,
@@ -24,6 +25,8 @@ static int handler(void* sqrt2, const char* section, const char* name,
         pconfig->loops = atoi(value);
     } else if (MATCH("sqrt2", "tolerance")) {
         pconfig->tolerance = atof(value);
+    } else if (MATCH("sqrt2", "numbers")) {
+    	sscanf(value, "%d,%d,%d", & pconfig->numbers[0], & pconfig->numbers[1], & pconfig->numbers[2]);
     } else {
         return 0;  /* unknown section/name, error */
     }
@@ -34,13 +37,18 @@ int main(void)
 {
 	configuration config;
 	pid_t pid;
+	int i;
 
     if (ini_parse("forksqrt.cfg", handler, &config) < 0) {
         printf("Can't load config'\n");
         return 1;
     }
-    printf("Config loaded from 'forksqrt.cfg': start=%d, loops=%d, tolerance=%.14f\n",
+    printf("Config loaded from 'forksqrt.cfg': start=%d, loops=%d, tolerance=%.14f, numbers=",
         config.start, config.loops, config.tolerance);
+    for(i = 0; i<3; i++) {
+    	printf("%d,", config.numbers[i]);
+    }
+    printf("\n");
     
     pid = fork();
     if(pid == 0)
