14102b3fb433f8494a29d9c0de00fb605681ce8b
Kirill Meng <kimeng@htwg-konstanz.de>
2016-11-12 17:26:37 +0100

    changed gentest2 to compile it

--- L2/gentest2.c
+++ L2/gentest2.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "genstacklib.h"
+
+void freefn(void* element);
+
+
+void freefn(void* element)
+{
+	
+}
 
 int main(void)
 {
-	void freefn(void* element)
-	{
 	
-	}
-
 	const char *words[]={"foo","bla","misc"};
 	genStack stringStack;
-
+	
 	GenStackNew (&stringStack, sizeof(char*), freefn);
 	for (int i=0; i<3;i++)
 	{
