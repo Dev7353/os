8c1963ac69dd5ee6ff7ca875c3422b216603057e
Kirill Meng <kimeng@htwg-konstanz.de>
2016-11-25 17:15:56 +0100

    indention gnu style

--- L3/matrix.c
+++ L3/matrix.c
@@ -10,20 +10,21 @@
 #define false 0
 #define true 1
 
-long diff(struct timespec t1, struct timespec t2);
+long diff (struct timespec t1, struct timespec t2);
 
-int main (int argc, char* argv[])
+int
+main (int argc, char *argv[])
 {
-	char* filea;
-	char* fileb;
+	char *filea;
+	char *fileb;
 	int multithreading = false;
 	int threads = 1;
-	int c;	
+	int c;
 
 	while ((c = getopt (argc, argv, "a:b:pt:")) != -1)
 	{
 		switch (c)
-	      {
+		{
 		case 'a':
 			filea = optarg;
 			break;
@@ -34,106 +35,116 @@ int main (int argc, char* argv[])
 			multithreading = true;
 			break;
 		case 't':
-			threads = atoi(optarg);
+			threads = atoi (optarg);
 			break;
 		case '?':
 			if (optopt == 'a')
-		  		fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				fprintf (stderr,
+					 "Option -%c requires an argument.\n",
+					 optopt);
 			if (optopt == 'b')
-		  		fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				fprintf (stderr,
+					 "Option -%c requires an argument.\n",
+					 optopt);
 			if (optopt == 't')
-		  		fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				fprintf (stderr,
+					 "Option -%c requires an argument.\n",
+					 optopt);
 
 			else if (isprint (optopt))
-		  		fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				fprintf (stderr, "Unknown option `-%c'.\n",
+					 optopt);
 			else
-		 		 fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
-        		return 1;
+				fprintf (stderr,
+					 "Unknown option character `\\x%x'.\n",
+					 optopt);
+			return 1;
 		default:
-			abort();
-	      }
+			abort ();
+		}
 
 	}
 
-	printf("FILE A \t\t %s\n", filea);
-	printf("FILE B \t\t %s\n", fileb);
-	char* mt;
-	if(multithreading == true)
+	printf ("FILE A \t\t %s\n", filea);
+	printf ("FILE B \t\t %s\n", fileb);
+	char *mt;
+	if (multithreading == true)
 		mt = "an";
 	else
 		mt = "aus";
-	printf("MULTITHREADING \t %s\n", mt);
-	printf("THREADS \t %d\n", threads);
+	printf ("MULTITHREADING \t %s\n", mt);
+	printf ("THREADS \t %d\n", threads);
 
 	Matrix *m1, *m2, *m3;
-	int i,j;
+	int i, j;
 	struct timespec t1, t2;
 
-	m1 = readMatrix(filea);
-	m2 = readMatrix(fileb);
-	
-	clock_gettime(CLOCK_MONOTONIC, &t1);
-	m3 = multiplyMatrix(m1, m2, threads);
-	clock_gettime(CLOCK_MONOTONIC, &t2);
+	m1 = readMatrix (filea);
+	m2 = readMatrix (fileb);
 
-	printf("Time : %ld [msec]\n", diff(t1, t2));	
+	clock_gettime (CLOCK_MONOTONIC, &t1);
+	m3 = multiplyMatrix (m1, m2, threads);
+	clock_gettime (CLOCK_MONOTONIC, &t2);
+
+	printf ("Time : %ld [msec]\n", diff (t1, t2));
 
 	FILE *fp;
-	fp = fopen("result", "w");
-	char *number = (char*) malloc(sizeof(long double));
+	fp = fopen ("result", "w");
+	char *number = (char *) malloc (sizeof (long double));
 	char tab = '\t';
 	char newline = '\n';
 
 	// copy the whole data from file  into matrix
-	for(i = 0; i < m3->rows; ++i)
+	for (i = 0; i < m3->rows; ++i)
 	{
-		for(j = 0; j < m3->rows; ++j)
+		for (j = 0; j < m3->rows; ++j)
 		{
-			sprintf(number, "%lf", m3->matrix[i][j]);
-			fputs(number, fp);
-			fputc(tab, fp);
+			sprintf (number, "%lf", m3->matrix[i][j]);
+			fputs (number, fp);
+			fputc (tab, fp);
 
 		}
-		fputc(newline, fp);
-		
+		fputc (newline, fp);
+
 	}
-	fclose(fp);
-	
+	fclose (fp);
+
 	// debug information 
 	int empty = 0;
-	for(i = 0; i < m3->rows; ++i)
+	for (i = 0; i < m3->rows; ++i)
 	{
-		for(j = 0; j < m3->rows; ++j)
+		for (j = 0; j < m3->rows; ++j)
 		{
-			if(m3->matrix[i][j] == 0)
+			if (m3->matrix[i][j] == 0)
 				++empty;
 		}
 	}
-	
-	printf("%d empty cells\n", empty); // if the number is high then maybe the algorithm isnt working 
-	
+
+	printf ("%d empty cells\n", empty);	// if the number is high then maybe the algorithm isnt working 
+
 	//free allocated memory
-	for(int i = 0; i < m3->rows; ++i)
+	for (int i = 0; i < m3->rows; ++i)
 	{
-		free(m1->matrix[i]);
-		free(m2->matrix[i]);
-		free(m3->matrix[i]);
+		free (m1->matrix[i]);
+		free (m2->matrix[i]);
+		free (m3->matrix[i]);
 	}
-	
-	free(m1->matrix);
-	free(m2->matrix);
-	free(m3->matrix);
-	
-	free(m1);
-	free(m2);
-	free(m3);
-	free(number);
+
+	free (m1->matrix);
+	free (m2->matrix);
+	free (m3->matrix);
+
+	free (m1);
+	free (m2);
+	free (m3);
+	free (number);
 }
 
-long diff(struct timespec t1, struct timespec t2)
+long
+diff (struct timespec t1, struct timespec t2)
 {
 	long t1nsec = t1.tv_sec * BILLION + t1.tv_nsec;
 	long t2nsec = t2.tv_sec * BILLION + t2.tv_nsec;
-	
+
 	return ((t2nsec - t1nsec) * MILLIONTH);
 }
--- L3/multiply_matrix.c
+++ L3/multiply_matrix.c
@@ -8,146 +8,156 @@
 
 typedef struct args
 {
-	Matrix *a; 
+	Matrix *a;
 	Matrix *b;
 	Matrix *result;
 	int start, stop, threads;
-			
+
 } args;
 
-void *calc(void *ar);
+void *calc (void *ar);
 
-Matrix *readMatrix(const char filename[])
+Matrix *
+readMatrix (const char filename[])
 {
 	FILE *fp;
 
-	fp =  fopen(filename, "r");
-	if(fp == NULL)
-		exit(1);
+	fp = fopen (filename, "r");
+	if (fp == NULL)
+		exit (1);
 
-	Matrix *m = (Matrix*) malloc(sizeof(Matrix));
-	if(m == NULL)
-		perror("malloc");	
-	
+	Matrix *m = (Matrix *) malloc (sizeof (Matrix));
+	if (m == NULL)
+		perror ("malloc");
+
 	int ch, cols = 0;
 
 	do
 	{
-		ch = fgetc(fp);	
-		if(ch == '\n')
+		ch = fgetc (fp);
+		if (ch == '\n')
 			++cols;
-	
-	} while(ch != EOF);
+
+	}
+	while (ch != EOF);
 
 	m->rows = cols;
 	m->columns = cols;
 
-	m->matrix = (double**) malloc(m->rows * sizeof(double*));
-	if(m->matrix == NULL)
-		perror("malloc");
-	for(int i = 0; i < m->columns;++i)
+	// allocate for double pinter to pointer and then a pointer for that pointer
+	m->matrix = (double **) malloc (m->rows * sizeof (double *));
+	if (m->matrix == NULL)
+		perror ("malloc");
+	for (int i = 0; i < m->columns; ++i)
 	{
-		m->matrix[i] = (double*) malloc(m->columns * sizeof(double));
-		if(m->matrix[i] == NULL)
-			perror("malloc");
+		m->matrix[i] =
+			(double *) malloc (m->columns * sizeof (double));
+		if (m->matrix[i] == NULL)
+			perror ("malloc");
 	}
-	fclose(fp);
-	fp =  fopen(filename, "r"); //reset file to the HEAD otherwise valgrind screams
+	fclose (fp);
+	fp = fopen (filename, "r");	//reset file to the HEAD otherwise valgrind screams
 
-	for(int i = 0; i < m->rows; ++i)
+	for (int i = 0; i < m->rows; ++i)
 	{
-			for(int j = 0; j < m->columns; ++j)
-			{
-				fscanf(fp, "%lf", &m->matrix[i][j]);
-			}
-	}	
-	
-	fclose(fp);
-	
+		for (int j = 0; j < m->columns; ++j)
+		{
+			fscanf (fp, "%lf", &m->matrix[i][j]);
+		}
+	}
+
+	fclose (fp);
+
 	return m;
 }
 
-Matrix *multiplyMatrix(Matrix *a, Matrix *b, int threads)
+Matrix *
+multiplyMatrix (Matrix * a, Matrix * b, int threads)
 {
-	Matrix *result = (Matrix*) malloc(sizeof(Matrix));
-	if(result == NULL)
-		perror("malloc");
-	
+	Matrix *result = (Matrix *) malloc (sizeof (Matrix));
+	if (result == NULL)
+		perror ("malloc");
+
 	result->rows = a->rows;
 	result->columns = a->columns;
 
-	if(result->rows != result->columns)
-		perror("The matrix multiplication support only nxn matrices\n");
+	if (result->rows != result->columns)
+		perror ("The matrix multiplication support only nxn matrices\n");
 
-	result->matrix = (double**) malloc(result->rows * sizeof(double*));
-	if(result->matrix == NULL)
-		perror("malloc");
+	result->matrix =
+		(double **) malloc (result->rows * sizeof (double *));
+	if (result->matrix == NULL)
+		perror ("malloc");
 
-	for(int i = 0; i < result->columns;++i)
+	for (int i = 0; i < result->columns; ++i)
 	{
-		result->matrix[i] = (double*) calloc(result->columns , sizeof(double));
-		if(result->matrix[i] == NULL)
-			perror("malloc");
+		result->matrix[i] =
+			(double *) calloc (result->columns, sizeof (double));
+		if (result->matrix[i] == NULL)
+			perror ("malloc");
 	}
-	
+
 	args ar[threads];
-	
-	for(int i = 0; i < threads; ++i)
+
+	for (int i = 0; i < threads; ++i)
 	{
 		ar[i].a = a;
 		ar[i].b = b;
 		ar[i].result = result;
 		ar[i].threads = threads;
 	}
-	
-	
+
+
 	pthread_t thread[threads];
 	errno = 0;
-	for(int i = 0; i < threads; ++i)
+	for (int i = 0; i < threads; ++i)
 	{
-		ar[i].start = i*(ar[i].a->rows/threads);
-		ar[i].stop = (ar[i].a->rows/threads)*(i+1);
-		if(pthread_create(&thread[i], NULL, calc, &ar[i]) != 0)
-		{	printf("ERRNO %d\n", errno);
-			perror("Thread creation failed\n");
-		}	
+		ar[i].start = i * (ar[i].a->rows / threads);
+		ar[i].stop = (ar[i].a->rows / threads) * (i + 1);
+		if (pthread_create (&thread[i], NULL, calc, &ar[i]) != 0)
+		{
+			printf ("ERRNO %d\n", errno);
+			perror ("Thread creation failed\n");
+		}
 	}
 
 
-	for(int i = 0; i < threads; ++i)
+	for (int i = 0; i < threads; ++i)
 	{
-		if(pthread_join(thread[i], NULL) != 0)
+		if (pthread_join (thread[i], NULL) != 0)
 		{
-			printf("ERRNO %d\n", errno);
+			printf ("ERRNO %d\n", errno);
 		}
 	}
 	return result;
 }
 
-double multiplyRowColumn(Matrix *a, int row, Matrix *b, int column)
+double
+multiplyRowColumn (Matrix * a, int row, Matrix * b, int column)
 {
 	double result = 0;
-	for(int k = 0; k < a->columns; ++k)
+	for (int k = 0; k < a->columns; ++k)
 	{
-		result += a->matrix[row][k] * b->matrix[k][column]; 			
+		result += a->matrix[row][k] * b->matrix[k][column];
 	}
-	
+
 	return result;
 }
 
-void* calc(void *ar)
+void *
+calc (void *ar)
 {
-	args *r = (args*) ar;	
-	for(int i = r->start; i < r->stop; ++i)
+	args *r = (args *) ar;
+	for (int i = r->start; i < r->stop; ++i)
 	{
-		for(int j = 0;j < r->a->rows; ++j)
+		for (int j = 0; j < r->a->rows; ++j)
 		{
 
-			r->result->matrix[i][j] = multiplyRowColumn(r->a, i, r->b, j);	
+			r->result->matrix[i][j] =
+				multiplyRowColumn (r->a, i, r->b, j);
 
 		}
 
 	}
 	return NULL;
 }
-
