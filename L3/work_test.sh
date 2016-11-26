81021d17fa4dbbe1d58bbdadc66c5c614be6dd0b
Kirill Meng <kimeng@htwg-konstanz.de>
2016-11-26 15:53:56 +0100

    safe expected execution time and real execution time in one line

--- L3/work_test.sh
+++ L3/work_test.sh
@@ -3,9 +3,7 @@ make clean
 make work
 for((i=1; i <= 10; i++))
 {
-    sec=$(shuf -i 0-9 -n 1)	
+    sec=$(shuf -i 1-9 -n 1)	
     msec=$(shuf -i 0-999 -n 1)
-    echo "./work $sec $msec"
-    time ./work $sec $msec >> rand_t.dat
+    echo "$sec$msec `./work $sec $msec | grep -o [0-9]*`" >> rand_t.dat
 }
-exit 0
