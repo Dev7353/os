2888951faf90b87461df382cf6f610d916d4c8bb
Tan Le-Minh <taleminh@htwg-konstanz.de>
2016-11-28 21:49:03 +0100

    Add documentation to functions and classes

--- L3/racecondition.py
+++ L3/racecondition.py
@@ -17,14 +17,24 @@ def usage():
     :return: The help string
     '''
     return """Usage: racecondition.py [OPTION]
+         This program increments a global variable a certain amount of times.
+         Without any given parameter, it runs it without any locks with
+         40 threads and 10 loops.
 
          -h, --help         	Displays this help
          -t, --threads       	Sets how often the global variable threads
                                 gets incremented (Default: 10)
-         -l, --loops          	Amount of threads to be created"""
+         -l, --loops          	Amount of threads to be created
+         -g, --gil 				Runs the program with GIL
+         -p, --peterson 		Runs the program with the peterson algorithm
+         -m, --mutex 			Runs the program with mutex"""
 
 
 def increment():
+    '''
+    Increments the global variable and sleeps for a delay to give control to
+    different thread
+    '''
     global global_var
     global delay
     tempvar = global_var
@@ -33,6 +43,9 @@ def increment():
 
 
 def race_lock(n_threads, n_loops):
+    '''
+    Executes the incrementing with GIL
+    '''
     exc = global_var + n_threads * n_loops
     threads = []
     # Consecutive for loops to guarantee sequential procedure
@@ -47,8 +60,10 @@ def race_lock(n_threads, n_loops):
 
 
 class init_thread(threading.Thread):
+    '''
+    Helper class to run incrementing with or without lock.
+    '''
     # Initialize the thread, overwrite to add arguments
-
     def __init__(self, n_loops, locked):
         threading.Thread.__init__(self)
         self.n_loops = n_loops
@@ -67,6 +82,9 @@ class init_thread(threading.Thread):
 
 
 def race(n_threads, n_loops):
+    '''
+    Executes the incrementing without locks
+    '''
     exc = global_var + n_threads * n_loops
     threads = []
     # Consecutive for loops to guarantee sequential procedure
@@ -81,6 +99,9 @@ def race(n_threads, n_loops):
 
 
 def race_p(n_threads, n_loops):
+    '''
+    Executes the incrementing with Peterson Algorithm
+    '''
     exc = global_var + n_threads * n_loops
     threads = []
     level = []
@@ -105,23 +126,28 @@ def race_p(n_threads, n_loops):
 
 
 def peterson(n_loops, n_threads, level, last_to_enter, i):
+    '''
+    Helper function to execute peterson algorithm.
+    '''
     k = 1
     for l in range(n_threads - 1):
         level[i] = l
         last_to_enter[l] = i
-        while(last_to_enter[l] == i and any((k != i and e >= l) for k, e in enumerate(level))):
-            if(k != i):
-                pass
+        while(last_to_enter[l] == i and any((k != i and j >= l) for k, j in enumerate(level))):
+            pass
         k += 1
     for x in range(n_loops):
         increment()
-    # print(global_var)
     level[i] = -1
     # http://www.csee.wvu.edu/~jdm/classes/cs550/notes/tech/mutex/Peterson2.html
     # https://en.wikipedia.org/wiki/Peterson%27s_algorithm
+    # https://docs.python.org/3/library/functions.html#any
 
 
 def race_m(n_loops, n_threads):
+    '''
+    Executes the incrementing with Mutex
+    '''
     exc = global_var + n_threads * n_loops
     global threadcount
     for i in range(n_threads):
@@ -137,6 +163,10 @@ def race_m(n_loops, n_threads):
 
 
 def mutex_lock(n_loops, mutex):
+    '''
+    Helper function for mutex
+    Acquires a lock and releases it once it's done incrementing.
+    '''
     global threadcount
     mutex.acquire()
     lockcount.append(mutex)
@@ -149,13 +179,11 @@ def mutex_lock(n_loops, mutex):
 
 
 def main():
-    print(global_var)
     threads = 40
     loops = 10
     gil = False
     mutex = False
     peterson = False
-    output = ""
     try:
         options, args = getopt.getopt(
             sys.argv[1:], 't:l:hgpm', ['threads', 'loops', 'help', 'gil',
@@ -186,14 +214,19 @@ def main():
             print(usage())
             break
     if gil is True:
-        output = str(race_lock(threads, loops))
+        result = race_lock(threads, loops)
     elif peterson is True:
-        output = str(race_p(threads, loops))
+        result = race_p(threads, loops)
     elif mutex is True:
-        output = str(race_m(threads, loops))
+        result = race_m(threads, loops)
     else:
-        output = str(race(threads, loops))
-    print(output)
+        result = race(threads, loops)
+
+    # Print results
+    print("After {} modifications, global_var should have become {}".format(
+        loops * threads, result[0]))
+    print("After {} modifications, global_var is {}".format(
+        loops * threads, result[1]))
 
 if __name__ == "__main__":
     main()
