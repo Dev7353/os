59a36a0ae2010090f748e866c08554655beccee2
Kirill Meng <kimeng@htwg-konstanz.de>
2016-11-28 14:29:46 +0100

    add gnu stlye and remove magic number

--- L3/work.c
+++ L3/work.c
@@ -6,61 +6,70 @@
 #define BILLION 1000000000
 #define MILLION 1000000
 #define MILLIONTH 0.000001
+#define MSEC
 
-void loop(long sec, long milisec);
-int gtd(struct timespec t1, struct timespec t2, long duration);
+void loop (long sec, long milisec);
+int gtd (struct timespec t1, struct timespec t2, long duration);
 
-int main(int argc, char* argv[])
+int
+main (int argc, char *argv[])
 {
-	clock_t m1, m2; // measure the real cpu time
+	clock_t m1, m2;		// measure the real cpu time
 	double elapse;
-	m1 =clock();
-	if(argc != 3)
+	m1 = clock ();
+	if (argc != 3)
 	{
-		fprintf(stderr, "To less or to many arguments");
+		fprintf (stderr, "To less or to many arguments");
 	}
 
-	int sec = atoi(argv[1]);
-	int milisec = atoi(argv[2]);
-	
-	if(sec > 10 & sec < 0)
-		fprintf(stderr, "Invalid range of seconds");
-	if(milisec > 999 & milisec < 0)
-		fprintf(stderr, "Invalid range of miliseconds");
-	
+	int sec = atoi (argv[1]);
+	int milisec = atoi (argv[2]);
+
+	if (sec > 10 & sec < 0)
+		fprintf (stderr, "Invalid range of seconds");
+	if (milisec > 999 & milisec < 0)
+		fprintf (stderr, "Invalid range of miliseconds");
+
 	struct timespec start, end;
-	
-	clock_gettime(CLOCK_MONOTONIC, &start);
-	loop(sec, milisec);
-	clock_gettime(CLOCK_MONOTONIC, &end);
-		
-	long diff = (end.tv_sec*BILLION + end.tv_nsec) - (start.tv_sec*BILLION + start.tv_nsec);
 
-	m2 = clock();
-	elapse = round((double) m2-m1)/1000;
-	printf("%ld %ld\n", (long)round(elapse), (long) (diff*MILLIONTH));
+	clock_gettime (CLOCK_MONOTONIC, &start);
+	loop (sec, milisec);
+	clock_gettime (CLOCK_MONOTONIC, &end);
+
+	long diff =
+		(end.tv_sec * BILLION + end.tv_nsec) -
+		(start.tv_sec * BILLION + start.tv_nsec);
+
+	m2 = clock ();
+	elapse = round ((double) m2 - m1) / MSEC;
+	printf ("%ld %ld\n", (long) round (elapse),
+		(long) (diff * MILLIONTH));
 	return 0;
-	
-} 
+
+}
 
 
-void loop(long sec, long milisec)
+void
+loop (long sec, long milisec)
 {
 	volatile int i;
-	
+
 	struct timespec begin, off;
-	clock_gettime(CLOCK_MONOTONIC, &begin);	
-	for(i = 0;;++i)
+	clock_gettime (CLOCK_MONOTONIC, &begin);
+	for (i = 0;; ++i)
 	{
-		clock_gettime(CLOCK_MONOTONIC, &off);
-		if(gtd(begin, off, (sec*BILLION + milisec*MILLION)) == 1)
+		clock_gettime (CLOCK_MONOTONIC, &off);
+		if (gtd (begin, off, (sec * BILLION + milisec * MILLION)) ==
+		    1)
 			break;
 	}
 }
 
-int gtd(struct timespec t1, struct timespec t2, long duration)
+int
+gtd (struct timespec t1, struct timespec t2, long duration)
 {
-	if(((t2.tv_sec*BILLION + t2.tv_nsec) - (t1.tv_sec*BILLION + t1.tv_nsec)) >= duration)
+	if (((t2.tv_sec * BILLION + t2.tv_nsec) -
+	     (t1.tv_sec * BILLION + t1.tv_nsec)) >= duration)
 		return 1;
 	return 0;
 }
