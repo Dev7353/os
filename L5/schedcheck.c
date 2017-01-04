69f096b5c1fd47af5cd87c7ee274b3ffa59c3b95
Kirill Meng <kimeng@htwg-konstanz.de>
2017-01-04 10:37:46 +0100

    add complete linux tests

--- L5/schedcheck.c
+++ L5/schedcheck.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sched.h>
 #include <errno.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 /*function prototypes*/
 int linux_scheduler_test();
@@ -33,36 +35,97 @@ int linux_scheduler_test()
 	struct sched_param param;
 	errno = 0;
 	
-	printf("================= Get scheduler policy test =================\n");
-	
+	printf("[Get scheduler policy test]\n");
 	int policy = sched_getscheduler(pid);
-	
 	if(policy == SCHED_OTHER)
-		printf("POLICY: the standard round-robin time-sharing policy\n");
+		printf("\tPOLICY: the standard round-robin time-sharing policy\n");
 	else if(policy == SCHED_RR)
-		printf("POLICY: a round-robin policy.\n");
+		printf("\tPOLICY: a round-robin policy.\n");
 	else if(policy == SCHED_FIFO)
-		printf("POLICY: a first-in, first-out policy\n");
+		printf("\tPOLICY: a first-in, first-out policy\n");
 	else
 	{
-		fprintf(stderr, "sched_getscheduler error occours.\n");
-		printf("ERRNO: %d\n", errno);
-		return -1;
+		fprintf(stderr, "\tsched_getscheduler error occours.\n");
+		printf("\tERRNO: %d\n", errno);
 	}
 		
 		
-	printf("================= Set scheduler policy test =================\n");
-
+	printf("[Set scheduler policy test]\n");
 	int ret = sched_setscheduler(0, SCHED_RR, &param);
 	if(ret != 0)
 	{
-		fprintf(stderr, "cannot set scheduler\n");
-		printf("ERRNO %d, retval: %d\n", errno, ret);
-		return -1;
+		fprintf(stderr, "\tcannot set scheduler\n");
+		printf("\tERRNO %d\n", errno);
+	} else
+		printf("\tset scheduler policy successfully\n");
+	
+	printf("[Get priority test]\n");
+	errno = 0;
+	int priority = 	getpriority(PRIO_PROCESS, 0); // 0 is the calling process
+	if(errno != 0)
+	{
+		printf("\tcannot get thr priority\n");
+		printf("\tERRNO: %d\n", errno);
 	}
+	printf("\tget priority successfully\n");
 	
-	printf("=================  test =================\n");
-
+	printf("[Set priority test]\n");
+	priority = -19; //highest prio
+	if(setpriority(PRIO_PROCESS, 0, priority) != 0)
+	{
+		fprintf(stderr, "\tcannot set priority\n");
+		printf("\tERRNO: %d\n", errno);
+	} else
+		printf("\tset priority successfully");
 	
+	printf("[Get param test]\n");
+	ret = sched_getparam(pid, &param);
+	if(ret != 0)
+	{
+		fprintf(stderr, "\tcannot get the scheduling parameter\n");
+		printf("\tERRNO: %d\n", errno);
+	} else
+		printf("\tget param successfully \n");
+	
+	printf("[Set param test]\n");
+	ret = sched_setparam(pid, &param);
+	if(ret != 0)
+	{
+		fprintf(stderr, "\tcannot set param\n");
+		printf("\tERRNO: %d\n", errno);
+	}else
+		printf("\tset param successfully\n");
+		
+	printf("[Get max scheduling priority of RR]\n");
+	errno = 0;
+	int max = sched_get_priority_max(SCHED_RR);
+	if(errno != 0)
+	{
+		fprintf(stderr, "\tcannot get the max scheduling priority\n");
+		printf("\tERRNO: %d\n", errno);
+	}else
+		printf("\tget the max priority successfully: %d\n", max);
+	
+	printf("[Get min scheduling priority of RR]\n");
+	errno = 0;
+	int min = sched_get_priority_min(SCHED_RR);
+	if(errno != 0)
+	{
+		fprintf(stderr, "\tcannot get the min scheduling priority\n");
+		printf("\tERRNO: %d\n", errno);
+	}else
+		printf("\tget the min priority successfully: %d\n", min);
+	
+	
+	printf("[Get RR interval test]\n");
+	errno = 0;
+	struct timespec iv;
+	ret = sched_rr_get_interval(pid, &iv);
+	if(ret != 0)
+	{
+		fprintf(stderr, "\tcannot get rr intervall\n");
+		printf("\tERRNO: %d\n", errno);
+	}else
+		printf("\tsuccessfully call, intervall is %ld s %lu ns\n", iv.tv_sec, iv.tv_nsec);
 	return 0;
 }
